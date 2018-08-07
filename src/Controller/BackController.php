be2e9275e93c498d825f3b01699256d34d5bf2b6
crownbackend <crownbackend@gmail.com>
2018-08-07 21:01:32 +0200

    integrate front

--- src/Controller/BackController.php
+++ src/Controller/BackController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller;
 
-
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,6 +9,10 @@ use Symfony\Component\Routing\Annotation\Route;
 class BackController extends Controller
 {
 
+    /**
+     * @Route("/admin", name="index_back_office", methods="GET")
+     * @return Response
+     */
     public function index(): Response {
 
         return $this->render('back/index.html.twig', [
--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -3,13 +3,54 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
 
 class FrontController extends Controller
 {
+
+    /**
+     * @Route("/", name="index", methods="GET")
+     * @return Response
+     */
     public function index(): Response {
 
         return $this->render('front/index.html.twig', [
             'title' => 'Accueil'
         ]);
     }
+
+    /**
+     * @Route("/boutique", name="shop", methods="GET")
+     * @return Response
+     */
+    public function shop(): Response {
+
+        return $this->render('front/shop.html.twig', [
+            'title' => 'Boutiques'
+        ]);
+    }
+
+    /**
+     * @Route("/soldes", name="soldes", methods="GET")
+     * @return Response
+     */
+    public function soldes(): Response {
+
+        return $this->render('front/soldes.html.twig', [
+            'title' => 'Retrouvez ici tous les soldes du moment'
+        ]);
+    }
+
+    /**
+     * @Route("/qui-sommes-nous", name="about", methods="GET")
+     * @return Response
+     */
+    public function about(): Response {
+
+        return $this->render('front/about.html.twig', [
+            'title' => 'Qui sommes nous ?'
+        ]);
+    }
+
 }
