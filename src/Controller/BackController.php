373c62cd2483b02289873af3df92e3ab6b5b7273
crownbackend <crownbackend@gmail.com>
2018-08-09 21:06:22 +0200

    add category

--- src/Controller/BackController.php
+++ src/Controller/BackController.php
@@ -2,15 +2,22 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
+use App\Form\CategoryType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/admin")
+ */
 
 class BackController extends Controller
 {
 
     /**
-     * @Route("/admin", name="index_back_office", methods="GET")
+     * @Route("/", name="index_back_office", methods="GET")
      * @return Response
      */
     public function index(): Response {
@@ -19,5 +26,36 @@ class BackController extends Controller
             'title' => 'Bienvenue dans votre back-Office'
         ]);
     }
+
+    /**
+     * @Route("/categorie/ajouter", name="add-category", methods="POST|GET")
+     * @param Request
+     * @return Response
+     */
+    public function addCategory(Request $request): Response {
+
+        $category = new Category();
+        $addCategory = $this->createForm(CategoryType::class, $category);
+        $addCategory->handleRequest($request);
+
+        if($addCategory->isSubmitted() && $addCategory->isValid()) {
+
+            $category = $addCategory->getData();
+            $manager = $this->getDoctrine()->getManager();
+            $manager->persist($category);
+            $manager->flush();
+
+            $this->addFlash('category', 'La catégorie à bien été ajouté.');
+            return $this->redirectToRoute('index_back_office');
+        }
+
+        return $this->render('back/CRUD/add-category.html.twig', [
+            'title' => 'Ajouter une categorie',
+            'addCategory' => $addCategory->createView()
+        ]);
+    }
+
+
+
 
 }
